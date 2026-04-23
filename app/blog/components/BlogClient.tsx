593140702fc966167231c29c37550c8b4583245c
WhiteTiger <nattawut.whitetiger@gmail.com>
2026-04-24 01:55:33 +0700

    [FIX] Suspense

---
+++ app/blog/components/BlogClient.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import FeaturedContent from "@/app/home/components/featuredContent"
+import SearchBar from "@/components/element/searchButton"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { useSearchParams } from "next/navigation"
+
+export default function BlogClient() {
+
+    const searchParams = useSearchParams()
+    const keyword = searchParams.get("search") || ""
+
+    return (
+        <div className="flex flex-col gap-6 px-4 py-5">
+
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+
+                <div className="lg:col-span-2 space-y-2">
+                    <Button variant="link" className="p-0">Hello</Button>
+
+                    <p className="text-3xl md:text-5xl font-bold">
+                        Explore our blog.
+                    </p>
+
+                    <p className="text-sm md:text-md text-muted-foreground max-w-xl">
+                        Discover insights, tutorials, and stories from developers and creators around the world.
+                    </p>
+                </div>
+
+                <div className="flex lg:justify-end items-end">
+                    <div className="w-full max-w-md">
+                        <SearchBar blogs={[]} />
+
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+
+                <div className="lg:col-span-2">
+                    <ScrollArea className="h-170 w-full pr-2">
+                        <FeaturedContent />
+                    </ScrollArea>
+                </div>
+
+                <div className="hidden md:flex flex-col gap-4">
+
+                    <Card className="w-full">
+                        <CardContent className="p-4">
+                            <p className="font-medium">Trending</p>
+                        </CardContent>
+                    </Card>
+
+                    <Card className="w-full">
+                        <CardContent className="p-4">
+                            <p>Latest tags</p>
+                        </CardContent>
+                    </Card>
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,69 +1,12 @@
-"use client"
+import React, { Suspense } from 'react'
+import BlogClient from './components/BlogClient'
 
-import SearchBar from "@/components/element/searchButton"
-import { Button } from "@/components/ui/button"
-import FeaturedContent from "../home/components/featuredContent"
-import { Card, CardContent } from "@/components/ui/card"
-import { ScrollArea } from "@/components/ui/scroll-area"
-import { useSearchParams } from "next/navigation"
+type Props = {}
 
-export default function BlogPage() {
-
-  const searchParams = useSearchParams()
-  const keyword = searchParams.get("search") || ""
-
+export default function BlogPage({ }: Props) {
   return (
-    <div className="flex flex-col gap-6 px-4 py-5">
-
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-
-        <div className="lg:col-span-2 space-y-2">
-          <Button variant="link" className="p-0">Hello</Button>
-
-          <p className="text-3xl md:text-5xl font-bold">
-            Explore our blog.
-          </p>
-
-          <p className="text-sm md:text-md text-muted-foreground max-w-xl">
-            Discover insights, tutorials, and stories from developers and creators around the world.
-          </p>
-        </div>
-
-        <div className="flex lg:justify-end items-end">
-          <div className="w-full max-w-md">
-            <SearchBar blogs={[]} />
-
-          </div>
-        </div>
-
-      </div>
-
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-
-        <div className="lg:col-span-2">
-          <ScrollArea className="h-170 w-full pr-2">
-            <FeaturedContent />
-          </ScrollArea>
-        </div>
-
-        <div className="hidden md:flex flex-col gap-4">
-
-          <Card className="w-full">
-            <CardContent className="p-4">
-              <p className="font-medium">Trending</p>
-            </CardContent>
-          </Card>
-
-          <Card className="w-full">
-            <CardContent className="p-4">
-              <p>Latest tags</p>
-            </CardContent>
-          </Card>
-
-        </div>
-
-      </div>
-
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <BlogClient />
+    </Suspense>
   )
 }
